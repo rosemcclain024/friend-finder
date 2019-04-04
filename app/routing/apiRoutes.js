8db88fe1238946baed4ae6457958e79934ccb59b
Rose McClain <rosemcclain024@gmail.com>
2019-04-04 18:26:26 -0400

    more logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,14 +6,31 @@ module.exports = function(app) {
 
     app.get("/api/friends", function(req, res) {
         res.json(friendsData);
+        console.log(app);
     });
 
     app.post("api/friends", function(req, res) {
         res.json(friendsData);
-    });
+        var userInput = req.body;
+        var totalDifference = 999;
+        var match;
 
+        for(i = 0; i < friendsData.length; i++) {
+            var differenceOfDataTaken = 0;
 
+            for(x = 0; x < friendsData[i].scores.length; x++) {
+                differenceOfDataTaken += Math.abs(userInput.scores[x]-friendsData[i].scores[x]);
+            }
+            if (differenceOfDataTaken < totalDifference) {
+                match = friendsData[i];
+                totalDifference = differenceOfDataTaken;
+            }
+        }
 
+    friendsData.push(userInput);
+    res.json(match);
+
+});
 
     app.get("*", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,16 @@
-
-
 var path = require("path");
+
+
 
 module.exports = function(app) {
 
-app.get("/api/survey", function(req, res) {
-    res.json(friendsData);
-  });
+    app.get("/api/survey", function(req, res) {
+        res.json(friendsData);
+    });
 
 
 
 
 
-  
+
 };
